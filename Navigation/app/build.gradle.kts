13c23ac419ae4733e17da3516f5ebd963035c458
Kostya <kgopaytsa@gmail.com>
2025-07-20 12:36:47 +0300

    clean MainActivity workpiece

--- Navigation/app/build.gradle.kts
+++ Navigation/app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    val nav_version = "2.9.2"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 }

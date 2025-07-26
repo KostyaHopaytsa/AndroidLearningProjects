8f8511479d3a6846435f17bb1bf10f2aa2c468f7
Kostya <kgopaytsa@gmail.com>
2025-07-26 23:20:52 +0300

    Tested runBlocking fun and added description to it

--- RunBlockingInCoroutines/app/src/main/java/com/example/runblockingincoroutines/MainActivity.kt
+++ RunBlockingInCoroutines/app/src/main/java/com/example/runblockingincoroutines/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.runblockingincoroutines
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,15 +14,46 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.runblockingincoroutines.ui.theme.RunBlockingInCoroutinesTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RunBlockingInCoroutinesTheme {
 
             }
         }
+
+        /*
+        runBlocking — міст між звичайним та асинхронним кодом.
+
+        Блокує потік, у якому викликається (зазвичай main-потік).
+        Виконує всі корутини всередині і чекає, поки вони завершаться.
+        Потрібен лише у звичайних (не suspend) функціях, щоб запустити корутину.
+
+        Краще не використовувати runBlocking в Android UI — він заморозить інтерфейс!
+        Замість нього є lifecycleScope / viewModelScope / LaunchedEffect.
+        */
+
+        Log.d(TAG, "Before run blocking")
+        runBlocking {
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "Finish IO coroutine1")
+            }
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "Finish IO coroutine2")
+            }
+            Log.d(TAG, "Start run blocking")
+            delay(5000L)
+            Log.d(TAG, "Start run blocking")
+        }
+        Log.d(TAG, "After run blocking")
     }
 }

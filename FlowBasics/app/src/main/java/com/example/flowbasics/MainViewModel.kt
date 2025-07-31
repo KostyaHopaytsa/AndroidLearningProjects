73afcf327ef447da192ddfd06809b5858ed09d26
Kostya <kgopaytsa@gmail.com>
2025-07-31 11:48:08 +0300

    Tried use flow operators such as filter, map, onEach, count, reduce, fold, flatMapConcat, buffer, conflate, collectLatest,

--- FlowBasics/app/src/main/java/com/example/flowbasics/MainViewModel.kt
+++ FlowBasics/app/src/main/java/com/example/flowbasics/MainViewModel.kt
@@ -3,14 +3,24 @@ package com.example.flowbasics
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.reduce
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.selects.whileSelect
 
 class MainViewModel: ViewModel() {
     val countDownFlow = flow<Int> {
-        val startingValue = 10
+        val startingValue = 5
         var currentValue = startingValue
         emit(startingValue)
         while(currentValue > 0) {
@@ -26,10 +36,79 @@ class MainViewModel: ViewModel() {
 
     private fun collectFlow() {
         viewModelScope.launch {
-            countDownFlow.collect { time ->
-                delay(1500L)
-                println("The current time is $time")
+//            val flow1 = flow {
+//                emit(1)
+//                delay(500L)
+//                emit(2)
+//            }
+//            viewModelScope.launch {
+//                flow1.flatMapConcat { value ->
+//                    flow {
+//                        emit(value + 1)
+//                        delay(500L)
+//                        emit(value + 2)
+//                    }
+//                }.collect { value ->
+//                    println("The value is $value")
+//                }
+//            }
+
+            val flow1 = flow {
+                delay(250L)
+                emit("Appetizer")
+                delay(1000L)
+                emit("Main dish")
+                delay(100L)
+                emit("Dessert")
             }
+            viewModelScope.launch {
+                flow1.onEach {
+                    println("FLOW: $it is delivered")
+                }
+//                    .buffer()     //дозволяє flow1 продовжувати роботу навіть якщо .collect не закінчив
+//                    .conflate()   // з .collectLatest дозволяє скіпати закінчення роботи
+//                                  // .collectLatest якщо вже почав працювати flow1
+                    .collect {
+                         println("FLOW: now eating $it")
+                        delay(1500L)
+                        println("FLOW: finished eating $it")
+                    }
+            }
+
+//            val count = countDownFlow
+//                .count {
+//                    it % 2 == 0
+//                }
+//            println("The count is $count")
+
+//            val reduceResult = countDownFlow
+//                .reduce { accumulator, value ->
+//                    accumulator + value
+//                }
+//            println("The reduce result is $reduceResult")
+
+//            val foldResult = countDownFlow
+//                .fold(100) { accumulator, value ->
+//                    accumulator + value
+//                }
+//            println("The fold result is $foldResult")
+
+//            countDownFlow
+//                .filter { time ->
+//                    time % 2 == 0
+//                }
+//                .map { time ->
+//                    time * time
+//                }
+//                .onEach { time ->
+//                    println(time)
+//                }
+//                .collect { time ->
+//                    println("The current time is $time")
+//
+//                }
         }
     }
 }
+
+

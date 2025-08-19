27cd50c7cafd8fb00635004c887dfb8d20ed6ed6
Kostya <kgopaytsa@gmail.com>
2025-08-19 21:07:44 +0300

    Implemented foregroundServices project to use PUSH_NOTIFICATION service

---
+++ ForegroundServices/app/src/main/java/com/example/foregroundservices/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.foregroundservices
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.app.ActivityCompat
+import com.example.foregroundservices.ui.theme.ForegroundServicesTheme
+import android.Manifest
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+        setContent {
+            ForegroundServicesTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        Intent(applicationContext, RunningService::class.java).also {
+                            it.action = RunningService.Actions.START.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text(text = "Start service")
+                    }
+                    Button(onClick = {
+                        Intent(applicationContext, RunningService::class.java).also {
+                            it.action = RunningService.Actions.STOP.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text(text = "Stop service")
+                    }
+                }
+            }
+        }
+    }
+}

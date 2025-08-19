c1e02047f35106c238c08545066e3d4bac2b234b
Kostya <kgopaytsa@gmail.com>
2025-08-19 14:16:52 +0300

    initial commit for BroadcastAndBroadcastReceivers project

---
+++ BroadcastAndBroadcastReceivers/app/src/main/java/com/example/broadcastandbroadcastreceivers/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.broadcastandbroadcastreceivers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.broadcastandbroadcastreceivers.ui.theme.BroadcastAndBroadcastReceiversTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BroadcastAndBroadcastReceiversTheme {
+
+            }
+        }
+    }
+}

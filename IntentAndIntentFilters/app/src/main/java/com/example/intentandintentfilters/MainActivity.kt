a9231e626e7f2d3b689f603c3613a4b67e8b6178
Kostya <kgopaytsa@gmail.com>
2025-08-19 12:58:40 +0300

    Implemented couple ways to use Intents and intent filters

--- IntentAndIntentFilters/app/src/main/java/com/example/intentandintentfilters/MainActivity.kt
+++ IntentAndIntentFilters/app/src/main/java/com/example/intentandintentfilters/MainActivity.kt
@@ -1,25 +1,77 @@
 package com.example.intentandintentfilters
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
 import com.example.intentandintentfilters.ui.theme.IntentAndIntentFiltersTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ImageViewModel> ()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             IntentAndIntentFiltersTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    viewModel.uri?.let{
+                        AsyncImage(
+                            model = viewModel.uri,
+                            contentDescription = null
+                        )
+                    }
+                    Button(onClick = {
+//                        Intent(applicationContext, SecondActivity::class.java).also {
+//                            startActivity(it)
+//                        }
 
+//                        Intent(Intent.ACTION_MAIN).also {
+//                            it.`package` = "com.google.android.youtube"
+//                            if(it.resolveActivity(packageManager) != null) {
+//                                startActivity(it)
+//                            }
+//                        }
+
+                        val intent = Intent(Intent.ACTION_SEND).apply {
+                            type = "text/plain"
+                            putExtra(Intent.EXTRA_EMAIL, arrayOf("test@test.com"))
+                            putExtra(Intent.EXTRA_SUBJECT, "My subject")
+                            putExtra(Intent.EXTRA_TEXT, "Content of my email")
+                        }
+                        if(intent.resolveActivity(packageManager) != null) {
+                            startActivity(intent)
+                        }
+                    }) {
+                        Text(text = "Click me")
+                    }
+                }
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM)
+        }
+        viewModel.updateUri(uri)
     }
 }
--- IntentAndIntentFilters/app/src/main/java/com/example/intentandintentfilters/SecondActivity.kt
+++ IntentAndIntentFilters/app/src/main/java/com/example/intentandintentfilters/SecondActivity.kt
@@ -3,14 +3,16 @@ package com.example.intentandintentfilters
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 import com.example.intentandintentfilters.ui.theme.IntentAndIntentFiltersTheme
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        intent.getStringExtra("")
         setContent {
             IntentAndIntentFiltersTheme {
-
+                Text(text = "SecondActivity")
             }
         }
     }
